87f5618508b5bc29c1c21efe6a469a7fd92e3bba
PiotrBerebecki <petebere@gmail.com>
2017-02-10 23:35:41 +0000

    Add ItemList component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import React, { Component } from 'react';
+
+import ItemList from './ItemList/index.js';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      items: null
+    };
+  }
 
+  componentDidMount() {
+    this.setState({
+      items: JSON.parse(localStorage.getItem('items')) || []
+    });
+  }
 
   render() {
+    console.dir(this.state.items);
     return (
 
       <div className="wrapper">
+
         <h2>YOUR FAVOURITES</h2>
-        <p></p>
-        <ul className="plates">
+        {/* <p></p> */}
+
+        <ItemList items={this.state.items} />
+
+        {/* <ul className="plates">
           <li>Loading favourites...</li>
-        </ul>
+        </ul> */}
+
         <form className="add-items" autoComplete="off">
           <input type="text" name="item" placeholder="Item Name" required />
           <input type="submit" value="+ Add Item" />
         </form>
+
       </div>
-      
-    )
+
+    );
   }
 }
 
